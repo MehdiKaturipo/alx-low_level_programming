18fe798087f07c3e2d698ea8c6d6bc66cdc3518b
Mehdi20 <soussan20mehdi@gmail.com>
2023-06-19 13:03:53 -0700

    19/6/2023

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,8 +1,8 @@
-[200~#include <stdio.h>
+#include <stdio.h>
 /**
- *  * main - main block
- *   * Return: 0
- *    */
+ * main - main block
+ * Return: 0
+ */
 int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
